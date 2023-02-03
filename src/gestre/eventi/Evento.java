a7a0c37f099ef5755d040795e58d922bf7bdf00a
LeppoXena <francesco.lepore@xenagames.com>
2023-02-03 10:35:57 +0100

    First commit

---
+++ src/gestre/eventi/Evento.java
@@ -0,0 +1,5 @@
+package gestre.eventi;
+
+public class Evento {
+
+}
---
+++ src/gestre/eventi/Main.java
@@ -0,0 +1,10 @@
+package gestre.eventi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
